2781731c272be75650d4ff988ec0de9e0abcf936
Marcio Dias <jmds.marcio@gmail.com>
2019-02-08 16:21:16 -0200

    Pedido, EstadoPagamento e Pagamento - Parte I

--- src/main/java/com/jmdsantos/cursomc/domain/Cliente.java
+++ src/main/java/com/jmdsantos/cursomc/domain/Cliente.java
@@ -37,6 +37,8 @@ public class Cliente implements Serializable{
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 		
 	}
@@ -105,7 +107,15 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
 
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -130,6 +140,4 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
 }
