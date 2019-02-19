37b2b2dda137230833c22e655ec8246baf3f9eef
Marcio Dias <jmds.marcio@gmail.com>
2019-02-19 10:54:03 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/jmdsantos/cursomc/Services/DBService.java
+++ src/main/java/com/jmdsantos/cursomc/Services/DBService.java
@@ -20,6 +20,7 @@ import com.jmdsantos.cursomc.domain.PagamentoComCartao;
 import com.jmdsantos.cursomc.domain.Pedido;
 import com.jmdsantos.cursomc.domain.Produto;
 import com.jmdsantos.cursomc.domain.enums.EstadoPagamento;
+import com.jmdsantos.cursomc.domain.enums.Perfil;
 import com.jmdsantos.cursomc.domain.enums.TipoCliente;
 import com.jmdsantos.cursomc.repositories.CategoriaRepository;
 import com.jmdsantos.cursomc.repositories.CidadeRepository;
@@ -124,15 +125,19 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "jose_dias@sicredi.com.br", "25233654121", TipoCliente.PESSOAFISICA, pe.encode("123") );
-		
 		cli1.getTelefones().addAll(Arrays.asList("33588328", "54526352"));
+		
+		Cliente cli2 = new Cliente(null, "Maria Silva", "jmds.marcio@gmail.com.br", "25233654121", TipoCliente.PESSOAFISICA, pe.encode("123") );
+		cli2.getTelefones().addAll(Arrays.asList("33588328", "54526352"));
+		
+		cli2.addPerfil(Perfil.ADMIN);
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "sala 800", "Centro", "38777012", cli1, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
